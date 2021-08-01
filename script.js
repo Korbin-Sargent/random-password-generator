1384a1482c0aeaebcd8eb63790ce1c72040a41d3
Korbin Sargent <korbinksargent@gmail.com>
2021-07-31 20:20:03 -0600

    Verified all code working and adding comments

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ var number = "0123456789";
 var special = "`!@#$%^&*()?";
 console.log(generateBtn);
 
-//function runs to generate a password
+//function runs to generate a password - need to possibly make
+//loop to prevent
 function generatePassword() {
   //creating a string of characters that the user would like to use in their password
   var userSelected = "";
@@ -15,54 +16,46 @@ function generatePassword() {
   );
   //changes userLength from a string to a numeric value
   userLength = Number(userLength);
-  if (userLength < 8) {
-    alert("password length must be atleast 8 characters long");
-    var userLength = prompt("Type a number between 8 and 128");
-    userLength = Number(userLength);
-  }
-  if (userLength > 128) {
-    alert("password length cannot exceed 128 characters long");
-    var userLength = prompt("Type a number between 8 and 128");
-    userLength = Number(userLength);
-  }
 
+  //if statement to ensure user enters a number 8 through 128. If not, an alert will
+  // pop up stating the error and the generatePassword function will start over again.
+  if (userLength < 8) {
+    alert("Password length must be atleast 8 characters long.");
+    return generatePassword();
+  }
+  console.log(typeof userLength);
+
+  if (userLength > 128) {
+    alert("Password length cannot exceed 128 characters in length.");
+    return generatePassword();
+  }
   //if statement is adding lowercase characters to our variable userSelected
   //if user chooses to include lowercase letters
-  var hasLowercase = confirm("Would you like to include lowercase numbers?");
+  var hasLowercase = confirm("Would you like to include lowercase letters?");
   if (hasLowercase) {
     userSelected = userSelected + lowercase;
   }
-  console.log(userSelected);
-
   //includes upper clase letters if user chooses to include on confirm pop up
   var hasUppercase = confirm("Would you like to include uppercase letters?");
   if (hasUppercase) {
     userSelected = userSelected + uppercase;
   }
-  console.log(userSelected);
 
   var hasNumber = confirm("Would you like to include numbers?");
   if (hasNumber) {
     userSelected = userSelected + number;
   }
-  console.log(userSelected);
-
+  //User has the option to add special characters to their randomly generated password.
   var hasSpecial = confirm("Would you like to include special characters?");
   if (hasSpecial) {
     userSelected = userSelected + special;
   }
-
-  console.log(userSelected);
-
-  // result += userSelected.charAt(Math.floor(Math.random() * userLength));
+  //this for loop chooses random characters from the userSelected
+  //character string to generates password
   var result = " ";
   for (var i = 0; i < userLength; i++) {
     result += userSelected.charAt(Math.floor(Math.random() * userLength));
   }
-
-  //this is where we actually generate the password. Use a loop
-  //what type of loop, how many times to loop, how to get random character from a string
-  //random index selection
   return result;
 }
 // Write password to the #password input
